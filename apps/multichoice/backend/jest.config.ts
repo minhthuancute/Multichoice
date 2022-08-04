ac689b54cd5d7ed2190ab17a0b0b6fd5f58946ab
Nath <dinhnhat.bui@zuhlke.com>
2022-08-04 09:56:49 +0700

    rename project

--- apps/multichoice/backend/jest.config.ts
+++ apps/multichoice/backend/jest.config.ts
@@ -1,7 +1,7 @@
 /* eslint-disable */
 export default {
-  displayName: 'backend',
-  preset: '../../jest.preset.js',
+  displayName: 'multichoice-backend',
+  preset: '../../../jest.preset.js',
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json',
@@ -12,5 +12,5 @@ export default {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/backend',
+  coverageDirectory: '../../../coverage/apps/multichoice/backend',
 };
--- apps/multichoice/frontend/jest.config.ts
+++ apps/multichoice/frontend/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'frontend',
-  preset: '../../jest.preset.js',
+  displayName: 'multichoice-frontend',
+  preset: '../../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': 'babel-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/frontend',
+  coverageDirectory: '../../../coverage/apps/multichoice/frontend',
 };
