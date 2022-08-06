a5eab67f67ba473e168d536a83bdb66debfe7e96
Nath <dinhnhat.bui@zuhlke.com>
2022-08-06 11:52:06 +0700

    add typeorm and user table

--- apps/multichoice/backend/src/app/app.module.ts
+++ apps/multichoice/backend/src/app/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { UserModule } from '../user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import configuration from '../config/configuration';
+import { OrmModule } from '../orm/orm.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ load: [configuration] }), UserModule],
+  imports: [
+    ConfigModule.forRoot({ load: [configuration] }),
+    OrmModule,
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
