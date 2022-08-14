24ef9710bae2ca5e2a29158ae0b1eafb719f91cd
Nath <dinhnhat.bui@zuhlke.com>
2022-08-14 10:11:23 +0700

    add validation user.dto

--- libs/multichoice/dto/src/lib/user.dto.ts
+++ libs/multichoice/dto/src/lib/user.dto.ts
@@ -1,32 +1,47 @@
-import { IsEmail, MinLength } from 'class-validator';
+import { IsEmail, MaxLength, MinLength } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
+import { validation } from '@monorepo/multichoice/validation';
 
 export class CreateUserDto {
   @ApiProperty()
-  @MinLength(4)
+  @MinLength(validation().username.minLength)
+  @MaxLength(validation().username.maxLength)
   username: string;
 
   @ApiProperty()
   @IsEmail()
+  @MaxLength(validation().email.maxLength)
   email: string;
 
   @ApiProperty()
-  @MinLength(6)
+  @MinLength(validation().password.minLength)
   password: string;
 }
 
 export class UpdateUserDto {
   @ApiProperty()
-  @MinLength(4)
+  @MinLength(validation().username.minLength)
+  @MaxLength(validation().username.maxLength)
   username: string;
+}
+
+export class UpdateUserPasswordDto {
+  @ApiProperty()
+  @MinLength(validation().password.minLength)
+  password: string;
+
+  @ApiProperty()
+  @MinLength(validation().password.minLength)
+  newPassword: string;
 }
 
 export class LoginUserDto {
   @ApiProperty()
   @IsEmail()
+  @MaxLength(validation().email.maxLength)
   email: string;
 
   @ApiProperty()
-  @MinLength(6)
+  @MinLength(validation().password.minLength)
   password: string;
 }
