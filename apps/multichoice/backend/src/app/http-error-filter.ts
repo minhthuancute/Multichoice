a474541479950da31ba862a6b14b0c02347e8fb4
Nath <dinhnhat.bui@zuhlke.com>
2022-08-18 11:17:23 +0700

    add default env

--- apps/multichoice/backend/src/app/http-error-filter.ts
+++ apps/multichoice/backend/src/app/http-error-filter.ts
@@ -8,7 +8,11 @@ import {
   BadRequestException,
   UnauthorizedException,
 } from '@nestjs/common';
-import { CannotCreateEntityIdMapError, EntityNotFoundError, QueryFailedError } from 'typeorm';
+import {
+  CannotCreateEntityIdMapError,
+  EntityNotFoundError,
+  QueryFailedError,
+} from 'typeorm';
 import { ErrorResponse } from '../model/ErrorResponse';
 import { Response } from 'express';
 
@@ -18,10 +22,10 @@ export class HttpErrorFilterr implements ExceptionFilter {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
-    let message = (exception as any).message.message;
+    let message = (exception as any).message;
     let code = 'HttpException';
 
-    // Logger.error(message, `${request.method} ${request.url}`);
+    Logger.error(message, `${request.method} ${request.url}`);
 
     let status: number = HttpStatus.INTERNAL_SERVER_ERROR;
 
@@ -29,33 +33,33 @@ export class HttpErrorFilterr implements ExceptionFilter {
       case HttpException:
         status = (exception as HttpException).getStatus();
         break;
-      case QueryFailedError:  // this is a TypeOrm error
-        status = HttpStatus.UNPROCESSABLE_ENTITY
+      case QueryFailedError: // this is a TypeOrm error
+        status = HttpStatus.UNPROCESSABLE_ENTITY;
         message = (exception as QueryFailedError).message;
         code = (exception as any).code;
         break;
-      case EntityNotFoundError:  // this is another TypeOrm error
-        status = HttpStatus.UNPROCESSABLE_ENTITY
+      case EntityNotFoundError: // this is another TypeOrm error
+        status = HttpStatus.UNPROCESSABLE_ENTITY;
         message = (exception as EntityNotFoundError).message;
         code = (exception as any).code;
         break;
       case CannotCreateEntityIdMapError: // and another
-        status = HttpStatus.UNPROCESSABLE_ENTITY
+        status = HttpStatus.UNPROCESSABLE_ENTITY;
         message = (exception as CannotCreateEntityIdMapError).message;
         code = (exception as any).code;
         break;
       case BadRequestException:
-        status = HttpStatus.BAD_REQUEST
+        status = HttpStatus.BAD_REQUEST;
         message = (exception as any).response.message;
         code = (exception as any).response.error;
         break;
       case UnauthorizedException:
-        status = HttpStatus.UNAUTHORIZED
+        status = HttpStatus.UNAUTHORIZED;
         message = (exception as any).response.message;
         code = (exception as any).response.message;
         break;
       default:
-        status = HttpStatus.INTERNAL_SERVER_ERROR
+        status = HttpStatus.INTERNAL_SERVER_ERROR;
     }
     response.status(status).json(ErrorResponse(status, message, code, request));
   }
