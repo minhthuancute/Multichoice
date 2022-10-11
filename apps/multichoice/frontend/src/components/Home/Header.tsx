5b67271737d3c9e7d077958fce9c2f083933d89a
minhthuancute <minhthuan140299@gmail.com>
2022-10-11 18:43:55 +0700

    change UI dropdown header

--- apps/multichoice/frontend/src/components/Home/Header.tsx
+++ apps/multichoice/frontend/src/components/Home/Header.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useRef, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { TOKEN } from '../../constants/contstants';
 import { classNames } from '../../helper/classNames';
 import { localServices } from '../../services/LocalServices';
@@ -7,10 +7,13 @@ import { userStore } from '../../store/rootReducer';
 import Navabar from '../Navbar/Navabar';
 import Logo from '../Logo/Logo';
 import Avatar from '../../assets/images/avatar.svg';
+import { useOnClickOutside } from '../../hooks/useOnClickOutside';
 
 const Header: React.FC = () => {
   const navigate = useNavigate();
   const { user } = userStore();
+
+  const refDropdownUser = useRef<HTMLDivElement>(null);
 
   const [openDropdown, setOpenDropdown] = useState<boolean>(false);
 
@@ -19,6 +22,11 @@ const Header: React.FC = () => {
     navigate('/login');
   };
 
+  const onClickOutsideDropdownUser = () => {
+    setOpenDropdown(false);
+  };
+
+  useOnClickOutside(refDropdownUser, onClickOutsideDropdownUser);
   return (
     <header
       className="bg-white"
@@ -47,21 +55,43 @@ const Header: React.FC = () => {
             </div>
             <div
               className={classNames(
-                `dropdown-user p-3 pt-20 bg-slate-50 shadow-md absolute right-0 top-full w-[248px] z-40
+                `dropdown-user p-4 bg-white shadow-md absolute right-0 top-full w-[296px] z-40
                 transition-all duration-200 transform scale-y-0 origin-top`,
                 {
                   'scale-y-100': openDropdown,
                 }
               )}
+              ref={refDropdownUser}
             >
-              <div className="logout">
-                <button
-                  className="btn-primary bg-primary-900 text-white text-sm w-full py-1 rounded-sm font-semibold"
+              <ul className="text-sm text-slate-800 mb-5">
+                <li className="mb-1.5 font-semibold text-base cursor-pointer">
+                  {user.username}
+                </li>
+                <li className="cursor-pointer">{user.email}</li>
+              </ul>
+              <ul className="pt-5 border-t border-slate-200 text-sm text-slate-800">
+                <Link
+                  to="/"
+                  className="block duration-200 transition-all
+                hover:font-semibold rounded-md mb-3"
+                >
+                  Quản lý tài khoản
+                </Link>
+                <Link
+                  to="/"
+                  className="block duration-200 transition-all
+                hover:font-semibold rounded-md mb-3"
+                >
+                  Đổi mật khẩu
+                </Link>
+                <li
+                  className="btn-primary bg-red-500 text-white text-sm w-full py-1
+                  rounded-sm font-semibold text-center pt-10 cursor-pointer border-t"
                   onClick={handleLogout}
                 >
-                  Logout
-                </button>
-              </div>
+                  Đăng Xuất
+                </li>
+              </ul>
             </div>
           </div>
         </div>
