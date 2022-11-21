58c834c1c418eef2bc46be6f1d1367ea7b4d23ed
Nath <dinhnhat.bui@zuhlke.com>
2022-11-21 12:00:01 +0700

    enhance filter topic

--- apps/multichoice/frontend/src/components/Tests/FilterTests.tsx
+++ apps/multichoice/frontend/src/components/Tests/FilterTests.tsx
@@ -2,57 +2,96 @@ import React from 'react';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import * as yup from 'yup';
-import { BiSearchAlt } from 'react-icons/bi';
 import Input from '../Commons/Input/Input';
 import { useSearchParams } from 'react-router-dom';
-
-const schemaFormFilter = yup.object().shape({
-  title: yup.string(),
-});
+import Select from 'react-select';
+import { TopicCategoryEnum } from '@monorepo/multichoice/constant';
 
 interface IFormFilterTest {
-  title: string; // keyword search Test
+  searchTerm: string; // keyword search Test
+  category: string; // category search
 }
 
 interface IFilterTests {
   onFilter?: (keyword: string) => void;
 }
 
+const schemaFormFilter = yup.object().shape({
+  searchTerm: yup.string().required(),
+  category: yup
+    .mixed<TopicCategoryEnum>()
+    .oneOf(Object.values(TopicCategoryEnum)),
+});
+
+const options = Object.values(TopicCategoryEnum).map((val) => {
+  return {
+    label: val,
+    value: val,
+  };
+});
+
 const FilterTests: React.FC<IFilterTests> = ({ onFilter }) => {
   const [searchParams, setSearchParams] = useSearchParams();
 
-  const { register, handleSubmit } = useForm<IFormFilterTest>({
+  const { register, handleSubmit, setValue } = useForm<IFormFilterTest>({
     resolver: yupResolver(schemaFormFilter),
   });
 
   const onSubmit: SubmitHandler<IFormFilterTest> = (
     formData: IFormFilterTest
   ) => {
-    setSearchParams('?search=' + formData.title);
+    setSearchParams('?search=' + formData.searchTerm);
     if (onFilter) {
-      onFilter(formData.title);
+      onFilter(formData.searchTerm);
     }
   };
-
   return (
     <div className="filter-test">
       <form
-        className="form-search-test flex items-center"
+        className="form-search-test flex items-center bg-white border border-solid border-stone-200 focus:border-primary-900 rounded-md"
         onSubmit={handleSubmit(onSubmit)}
       >
         <Input
-          className="w-1/3"
+          className="flex-1"
           placeholder="Tìm kiếm đề thi"
-          registerField={register('title')}
+          registerField={register('searchTerm')}
           inputSize="md"
           defaultValue={searchParams.get('search')}
+          hasBorder={false}
         />
-        <button
-          className="bg-slate-500 text-white py-3.5 px-4 rounded-md ml-1
-        transition-all duration-200 focus:ring focus:ring-slate-100"
-        >
-          <BiSearchAlt />
-        </button>
+        <div className="h-6 bg-stone-200 w-[1px]"></div>
+        <Select
+          components={{
+            IndicatorSeparator: () => null,
+          }}
+          menuPlacement="auto"
+          isSearchable={false}
+          options={options}
+          onChange={(option) => {
+            setValue('category', option?.value || '');
+          }}
+          placeholder="Chọn danh mục..."
+          className="text-sm"
+          styles={{
+            menu: (base) => ({
+              ...base,
+              width: 'max-content',
+              minWidth: '100%',
+            }),
+            control: (base) => ({
+              ...base,
+              border: '0 !important',
+              // This line disable the blue border
+              boxShadow: '0 !important',
+              '&:hover': {
+                border: '0 !important',
+              },
+              textAlign: 'right',
+              position: 'static',
+              transform: 'none',
+            }),
+          }}
+        />
       </form>
     </div>
   );
