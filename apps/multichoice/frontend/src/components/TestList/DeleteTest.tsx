49874f5392cab435709048e86a2ac54d879e8b2a
Minh Thuận <48424223+minhthuancute@users.noreply.github.com>
2022-11-21 21:10:47 +0700

    fix: question type multi (#113)

--- apps/multichoice/frontend/src/components/TestList/DeleteTest.tsx
+++ apps/multichoice/frontend/src/components/TestList/DeleteTest.tsx
@@ -41,15 +41,17 @@ const DeleteTest: React.FC<IDeleteTest> = ({
       setOpenModal={setOpenModalDelete}
       placement="CENTER"
     >
-      <div className="modal-content px-5 flex flex-col justify-center bg-white rounded-md max-w-lg w-full h-60">
+      <div className="modal-content px-5 flex flex-col justify-center bg-white rounded-md max-w-lg w-full py-8">
         <div className="header text-center">
-          <RiErrorWarningLine className="text-red-600 text-5xl mx-auto" />
+          <h2 className="text-slate-800 text-xl font-semibold capitalize">
+            Xoá bỏ đề thi
+          </h2>
           <h4 className="mt-4 text-slate-800 text-tiny">
             Bạn có chắc chắn muốn xóa bỏ đề thi:{' '}
             <span className="font-semibold">{testTitle}</span> ?
           </h4>
         </div>
-        <div className="body ctas flex items-center justify-center gap-x-2 mt-12">
+        <div className="body ctas flex items-center justify-center gap-x-2 mt-10">
           <button
             className="create-test rounded-md flex justify-center items-center w-32 h-10 text-sm
           text-slate-800 font-bold border border-solid border-slate-800 focus:ring focus:ring-slate-100"
