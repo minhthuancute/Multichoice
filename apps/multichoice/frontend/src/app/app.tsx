ba7c739d420878330ed6321b5784ef5e210de1b6
minhthuancute <minhthuan140299@gmail.com>
2022-12-15 18:59:49 +0700

    ST-70: fix calling api /token in login page

--- apps/multichoice/frontend/src/app/app.tsx
+++ apps/multichoice/frontend/src/app/app.tsx
@@ -1,25 +1,30 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import React, { useEffect } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 import { ReactNotifications } from 'react-notifications-component';
 import AppRoutes from '../components/Routes/AppRoutes';
 import Splash from '../components/Splash/Splash';
 import { userStore } from '../store/rootReducer';
 
-export const App: React.FC = () => {
-  const { checkAuthenticated } = userStore();
+const App: React.FC = () => {
+  const { checkAuthenticated, isAuthenticated } = userStore();
+  const location = useLocation();
 
   useEffect(() => {
-    checkAuthenticated();
+    const currentPath = location.pathname;
+    const blackList = ['/login', '/register'];
+    if (!blackList.includes(currentPath) && !isAuthenticated) {
+      checkAuthenticated();
+    }
   }, []);
 
   return (
-    <BrowserRouter>
+    <>
       <ReactNotifications />
       <Splash />
       <AppRoutes />
-    </BrowserRouter>
+    </>
   );
 };
 
